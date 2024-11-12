41263c161d8725d209f4b9c5cc8af24bbb0af6d2
Navya Sri Alugubilli <alugubillinavya@gmail.com>
2024-11-12 18:08:26 -0500

    deploy

--- src/Kanbas/Accounts/Navigation.tsx
+++ src/Kanbas/Accounts/Navigation.tsx
@@ -6,18 +6,8 @@ export default function AccountNavigation() {
   const { pathname } = useLocation();
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      {links.map((link) => (
-        <>
-        <Link
-          key = {link}
-          id={"wd-account-signin-link"}
-          to={`/Kanbas/Account/${link}`}
-          className={`list-group-item active border border-0 ${pathname.includes(link)? "active" : "text-danger"}`}
-        >
-          {link}{" "}
-        </Link> {" "}
-        <br />
-        </>
-      ))}
+      <Link id="wd-account-signin-link" to={`/Kanbas/Account/Signin`} className="list-group-item active border border-0" > Signin  </Link> <br/>
+      <Link id="wd-account-signup-link" to={`/Kanbas/Account/Signup`} className="list-group-item text-danger border border-0" > Signup  </Link> <br/>
+      <Link id="wd-account-profile-link" to={`/Kanbas/Account/Profile`} className="list-group-item text-danger border border-0" > Profile </Link> <br/>
     </div>
 );}
